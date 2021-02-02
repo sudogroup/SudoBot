6c902f1106b0d864d590979b5a1b9389254d4377
Mutlaq Aldhbuiub <matulkn@gmail.com>
2021-02-02 15:43:14 +0300

    remove channel command

---
+++ commands/rmcha.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: "rmcha",
+    description:
+        "Remove a channel",
+    args: true,
+    usage: "<channel_name>",
+    moderator: true,
+    async execute(msg, args) {
+        // get parameters
+        const channel_name = args[0];
+
+
+        let channel_id = msg.guild.channels.cache.find(channel => channel.name === channel_name).id;
+
+        if(channel_id == null || channel_id == undefined){
+            return msg.channel.send(`❌ There's no channel with this name, try again! `);
+        }
+        
+        const fetchedChannel = msg.guild.channels.cache.get(channel_id);
+
+        if(fetchedChannel.delete()){
+            return msg.channel.send(`✅ The channel has been deleted successfully`);
+        }
+    },
+};
